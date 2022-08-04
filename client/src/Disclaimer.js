c0d3519379bf13ceffaa81527ec5a401381444da
CatherineHonors <cathonors@gmail.com>
2022-08-04 15:43:21 -0400

    hover effect on contact page and formatted privacy policy

--- client/src/Disclaimer.js
+++ client/src/Disclaimer.js
@@ -1,57 +1,60 @@
 import React from "react";
 
-
 function Disclaimer() {
 
     return (
-        <div class="card">
-            <div class="card-body text-center">
-                <h1 class="card-title">Site Disclaimer</h1>
+        <div class="card w-75 mx-auto" style={{ padding: 20, paddingTop: 0, marginTop: 30}}>
+            <div class="card-body text-center" >
+                {/* <h1 class="card-title">Site Disclaimer</h1> */}
                 <br></br>
-                <p class="card-text">Legal Use and Copyrights
-
-                    Any person accessing this site agrees to the following:
-
-                    COPYRIGHT:
-                    All materials contained in this Website are the copyrighted property of The Gregory Weiner Foundation (the “Authors and Publishers”).
-
-                    Any person is authorized to use, copy, and distribute any complete page or, where a document consists of more than one page, any complete document contained on this Website, including related graphics, subject to the following conditions:
-                    1. Copying or distributing less than a complete page or (where a document consists of more than page) the complete document is prohibited;
-                    2. The copyright notice set forth above and this permission notice must appear on all copies;
-                    3. Use, copying, and distribution shall be solely for informational, non-commercial purposes; and
-                    4. No graphics elements on any page or document on this Website may be used, copied, or distributed separate from the accompanying text.
-
-                    NO REPRESENTATION OR WARRANTIES:
-
-                    LIMITATIONS ON LIABILITY:
-                    THE INFORMATION AND MATERIALS ON THIS WEBSITE COULD INCLUDE TECHNICAL INACCURACIES OR TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY MADE TO THE INFORMATION CONTAINED HEREIN. THE AUTHORS AND PUBLISHERS MAY MAKE IMPROVEMENTS AND/OR CHANGES IN THE PRODUCT(S) AND/OR THE PROGRAM(S) DESCRIBED HEREIN AT ANY TIME. THE AUTHORS AND PUBLISHERS MAKE NO REPRESENTATION OR WARRANTIES WITH RESPECT TO ANY INFORMATION, MATERIALS OR GRAPHICS ON THIS WEBSITE, ALL OF WHICH IS PROVIDED ON A STRICTLY “AS IS” BASIS, WITHOUT WARRANTY OF ANY KIND. THE AUTHORS AND PUBLISHERS AND/OR THEIR RESPECTIVE SUPPLIERS HEREBY EXPRESSLY DISCLAIM ALL WARRANTIES WITH REGARD TO ANY INFORMATION, MATERIALS OR GRAPHICS ON THIS WEBSITE, INCLUDING ALL WARRANTIES OF MERCHANTIBILITY, FITNESS FOR A PARTICUALR PURPOSE, AND NON-INFRINGEMENT. ANY CLAIM WITH RESPECT TO THE MATERIALS CONTAINED ON THIS WEBSITE SHALL BE GOVERNED BY, AND CONSTRUED IN ACCORDANCE WITH, THE LAWS OF THE COMMONWEALTH OF VIRGINIA. UNDER NO CIRCUMSTANCES, SHALL THE AUTHORS AND PUBLISHERS, THEIR EMPLOYEES, AFFILIATES, AND/OR THEIR RESPECTIVE SUPPLIERS BE LIABLE UNDER ANY THEORY OF RECOVERY, AT LAW OR IN EQUITY, FOR ANY DAMAGES, INCLUDING WITHOUT LIMITATION, SPECIAL, DIRECT, INCIDENTAL, CONSEQUENTIAL OR PUNITIVE DAMAGES (INCLUDING, BUT NOT LIMITED TO LOSS OF OR USE OR LOST PROFITS) ARISING OUT OF OR IN ANY MANNER CONNECTED WITH THE USE, OR PERFORMANCE OF, INFORMATION, SERVICES, SOFTWARE, OR DOCUMENTS, OR THE FAILURE TO PROVIDE INFORMATION OR SERVICES, FROM THE SITE.
-
-                    LINKS TO THIRD PARTY WEBSITES:
-                    The Authors and Publishers may provide hyperlinks to third party websites as a convenience to users of this Website. The Authors and Publishers do not control third party websites and are not responsible for the contents of any hyperlinked third party websites or any hyperlink in a hyperlinked site. The Authors and Publishers do not endorse, recommend, or approve any third party website hyperlinked from this Website. The Authors and Publishers shall have no liability to any entity for the content or use of the content available through such hyperlink.
-
-                    THE GREGORY WEINER FOUNDATION SERVICE MARKS:
-
-                    The Gregory Weiner Foundation and other service marks (including logo and designs) found on this Website are service marks that identify The Gregory Weiner Foundation and the services provided by the Gregory Weiner Foundation.  Such service marks may not be used under any circumstances without prior written authorization.
-
-                    PRIVACY POLICY
-
-                    The Gregory Weiner Foundation respects the privacy of your personal identifying information (“Information”) and has provided this explanation about our online information practices as a show of our commitment to protect your privacy. The Gregory Weiner Foundation does not share or in any other way use information collected during your use of our Website without your permission except as outlined in this policy.  The Gregory Weiner Foundation does not currently collect information except; (1) information provided by your web browser to our web server, including such information as the site you linked from and your email address, and (2) information provided by you in an email message addressed to us.
-                    The Gregory Weiner Foundation only uses information for the purpose of which the information was submitted or to evaluate the effectiveness of this website.
-
-                    COOKIES:
-                    The Gregory Weiner Foundation may decide to collect information submitted voluntarily through cookies. “Cookies” are used to store and sometimes track your preferences in order to provide you with customized and personalized services.  A cookie is a data sent to your Internet browser from a web server and stored on your computer hard drive. Cookies make your web experience easier by storing passwords and preferences.  Cookies are used by most Internet websites.  You can adjust your browser settings to refuse all cookies or to inform you when a cookie is being placed on your hard drive. However, your election not to accept cookies may diminish your experience with websites because of additional time needed to repeatedly enter personal identifying information.
-
-                    COPYRIGHT NOTICE:
-
-                    Copyright © 2014.  The Gregory Weiner Foundation. All rights reserved.
-
-                    TRADEMARKS :
-
-                    Product and company names mentioned herein may be the trademarks of their respective owners. Any rights not expressly granted herein are reserved.
-                    This agreement is effective until terminated by either party. You may terminate this agreement at any time by discontinuing your use of this website and destroying all materials obtained from it.
-                    The Gregory Weiner Foundation reserves the right to change this policy at any time by posting a new privacy policy at this location.
-
-                    You may send an e-mail to us with any further questions: admin@gregoryweiner.org</p>
+                <p class="card-text"><b>Legal Use and Copyrights</b>
+                <br></br>
+                Any person accessing this site agrees to the following:
+                <br></br>
+                <br></br>
+                <b>COPYRIGHT</b>
+                <br></br>
+                All materials contained in this Website are the copyrighted property of The Gregory Weiner Foundation (the “Authors and Publishers”).
+                <br></br>
+                Any person is authorized to use, copy, and distribute any complete page or, where a document consists of more than one page, any complete document contained on this Website, including related graphics, subject to the following conditions:
+                    <ol class="list-group list-group-numbered w-50 mx-auto">
+                        <li class="list-group-item">Copying or distributing less than a complete page or (where a document consists of more than page) the complete document is prohibited;</li>
+                        <li class="list-group-item">The copyright notice set forth above and this permission notice must appear on all copies;</li>
+                        <li class="list-group-item">Use, copying, and distribution shall be solely for informational, non-commercial purposes; and</li>
+                        <li class="list-group-item">No graphics elements on any page or document on this Website may be used, copied, or distributed separate from the accompanying text.</li>
+                    </ol>
+                <br></br>
+                <b>NO REPRESENTATION OR WARRANTIES </b>
+                <br></br>
+                <br></br>
+                <b>LIMITATIONS ON LIABILITY:</b>
+                <br></br>
+                THE INFORMATION AND MATERIALS ON THIS WEBSITE COULD INCLUDE TECHNICAL INACCURACIES OR TYPOGRAPHICAL ERRORS. CHANGES ARE PERIODICALLY MADE TO THE INFORMATION CONTAINED HEREIN. THE AUTHORS AND PUBLISHERS MAY MAKE IMPROVEMENTS AND/OR CHANGES IN THE PRODUCT(S) AND/OR THE PROGRAM(S) DESCRIBED HEREIN AT ANY TIME. THE AUTHORS AND PUBLISHERS MAKE NO REPRESENTATION OR WARRANTIES WITH RESPECT TO ANY INFORMATION, MATERIALS OR GRAPHICS ON THIS WEBSITE, ALL OF WHICH IS PROVIDED ON A STRICTLY “AS IS” BASIS, WITHOUT WARRANTY OF ANY KIND. THE AUTHORS AND PUBLISHERS AND/OR THEIR RESPECTIVE SUPPLIERS HEREBY EXPRESSLY DISCLAIM ALL WARRANTIES WITH REGARD TO ANY INFORMATION, MATERIALS OR GRAPHICS ON THIS WEBSITE, INCLUDING ALL WARRANTIES OF MERCHANTIBILITY, FITNESS FOR A PARTICUALR PURPOSE, AND NON-INFRINGEMENT. ANY CLAIM WITH RESPECT TO THE MATERIALS CONTAINED ON THIS WEBSITE SHALL BE GOVERNED BY, AND CONSTRUED IN ACCORDANCE WITH, THE LAWS OF THE COMMONWEALTH OF VIRGINIA. UNDER NO CIRCUMSTANCES, SHALL THE AUTHORS AND PUBLISHERS, THEIR EMPLOYEES, AFFILIATES, AND/OR THEIR RESPECTIVE SUPPLIERS BE LIABLE UNDER ANY THEORY OF RECOVERY, AT LAW OR IN EQUITY, FOR ANY DAMAGES, INCLUDING WITHOUT LIMITATION, SPECIAL, DIRECT, INCIDENTAL, CONSEQUENTIAL OR PUNITIVE DAMAGES (INCLUDING, BUT NOT LIMITED TO LOSS OF OR USE OR LOST PROFITS) ARISING OUT OF OR IN ANY MANNER CONNECTED WITH THE USE, OR PERFORMANCE OF, INFORMATION, SERVICES, SOFTWARE, OR DOCUMENTS, OR THE FAILURE TO PROVIDE INFORMATION OR SERVICES, FROM THE SITE.
+                <br></br>
+                <br></br>
+                LINKS TO THIRD PARTY WEBSITES:
+                The Authors and Publishers may provide hyperlinks to third party websites as a convenience to users of this Website. The Authors and Publishers do not control third party websites and are not responsible for the contents of any hyperlinked third party websites or any hyperlink in a hyperlinked site. The Authors and Publishers do not endorse, recommend, or approve any third party website hyperlinked from this Website. The Authors and Publishers shall have no liability to any entity for the content or use of the content available through such hyperlink.
+                <br></br>
+                THE GREGORY WEINER FOUNDATION SERVICE MARKS:
+                The Gregory Weiner Foundation and other service marks (including logo and designs) found on this Website are service marks that identify The Gregory Weiner Foundation and the services provided by the Gregory Weiner Foundation.  Such service marks may not be used under any circumstances without prior written authorization.
+                <br></br>
+                <br></br>
+                <b>PRIVACY POLICY </b>
+                <br></br>
+                The Gregory Weiner Foundation respects the privacy of your personal identifying information (“Information”) and has provided this explanation about our online information practices as a show of our commitment to protect your privacy. The Gregory Weiner Foundation does not share or in any other way use information collected during your use of our Website without your permission except as outlined in this policy.  The Gregory Weiner Foundation does not currently collect information except; (1) information provided by your web browser to our web server, including such information as the site you linked from and your email address, and (2) information provided by you in an email message addressed to us. The Gregory Weiner Foundation only uses information for the purpose of which the information was submitted or to evaluate the effectiveness of this website.
+                <br></br>
+                COOKIES:
+                The Gregory Weiner Foundation may decide to collect information submitted voluntarily through cookies. “Cookies” are used to store and sometimes track your preferences in order to provide you with customized and personalized services.  A cookie is a data sent to your Internet browser from a web server and stored on your computer hard drive. Cookies make your web experience easier by storing passwords and preferences.  Cookies are used by most Internet websites.  You can adjust your browser settings to refuse all cookies or to inform you when a cookie is being placed on your hard drive. However, your election not to accept cookies may diminish your experience with websites because of additional time needed to repeatedly enter personal identifying information.
+                <br></br>
+                COPYRIGHT NOTICE: Copyright © 2014.  The Gregory Weiner Foundation. All rights reserved.
+                <br></br>
+                TRADEMARKS :
+                Product and company names mentioned herein may be the trademarks of their respective owners. Any rights not expressly granted herein are reserved.
+                This agreement is effective until terminated by either party. You may terminate this agreement at any time by discontinuing your use of this website and destroying all materials obtained from it.
+                The Gregory Weiner Foundation reserves the right to change this policy at any time by posting a new privacy policy at this location.
+                <br></br>
+                <br></br>
+                You may send an e-mail to us with any further questions: <b>admin@gregoryweiner.org</b></p>
             </div>
         </div>
     );
